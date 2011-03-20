c002e7717870952f66f8d2c3d961f5add8d7530b
wdl1908@gmail.com <wdl1908@gmail.com@752ffa67-3f75-dc4c-a3bb-520686bced8d>
2011-03-20 13:32:54 +0000

    Icon creation with svg files

---
+++ Icons/stat_wifi/mk_stat_wifi.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+. ../library/library.sh
+
+TEMPLATE=stat_sys_wifi_signal_0_template.svg
+mkicon $TEMPLATE stat_sys_wifi_signal_0.svg
+
+TEMPLATE=stat_notify_wifi_in_range_template.svg
+mkicon $TEMPLATE stat_notify_wifi_in_range0.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#00aaff' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#00aaff'
+mkicon $TEMPLATE stat_notify_wifi_in_range1.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#00aaff' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#00aaff' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#00aaff'
+mkicon $TEMPLATE stat_notify_wifi_in_range2.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#00aaff' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#00aaff' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#00aaff' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#00aaff'
+mkicon $TEMPLATE stat_notify_wifi_in_range3.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#00aaff' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#00aaff' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#00aaff' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#00aaff' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#00aaff'
+mkicon $TEMPLATE stat_notify_wifi_in_range4.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#00aaff' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#00aaff' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#00aaff' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#00aaff'
+mkicon $TEMPLATE stat_notify_wifi_in_range5.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#00aaff' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#00aaff' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#00aaff'
+
+TEMPLATE=stat_sys_wifi_signal_template.svg
+mkicon $TEMPLATE stat_sys_wifi_signal_1_anim0.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ff0000' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ff0000'
+mkicon $TEMPLATE stat_sys_wifi_signal_1_anim1.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ff0000' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ff0000' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#ff0000'
+mkicon $TEMPLATE stat_sys_wifi_signal_1_anim2.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ff0000' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ff0000' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#ff0000' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#ff0000'
+mkicon $TEMPLATE stat_sys_wifi_signal_1_anim3.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ff0000' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ff0000' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#ff0000' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#ff0000' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#ff0000'
+mkicon $TEMPLATE stat_sys_wifi_signal_1_anim4.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ff0000' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ff0000' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#ff0000' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#ff0000'
+mkicon $TEMPLATE stat_sys_wifi_signal_1_anim5.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ff0000' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ff0000' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#ff0000'
+
+mkicon $TEMPLATE stat_sys_wifi_signal_2_anim0.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ffaa00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ffaa00'
+mkicon $TEMPLATE stat_sys_wifi_signal_2_anim1.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ffaa00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ffaa00' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#ffaa00'
+mkicon $TEMPLATE stat_sys_wifi_signal_2_anim2.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ffaa00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ffaa00' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#ffaa00' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#ffaa00'
+mkicon $TEMPLATE stat_sys_wifi_signal_2_anim3.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ffaa00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ffaa00' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#ffaa00' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#ffaa00' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#ffaa00'
+mkicon $TEMPLATE stat_sys_wifi_signal_2_anim4.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ffaa00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ffaa00' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#ffaa00' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#ffaa00'
+mkicon $TEMPLATE stat_sys_wifi_signal_2_anim5.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ffaa00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ffaa00' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#ffaa00'
+
+mkicon $TEMPLATE stat_sys_wifi_signal_3_anim0.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ffff00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ffff00'
+mkicon $TEMPLATE stat_sys_wifi_signal_3_anim1.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ffff00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ffff00' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#ffff00'
+mkicon $TEMPLATE stat_sys_wifi_signal_3_anim2.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ffff00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ffff00' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#ffff00' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#ffff00'
+mkicon $TEMPLATE stat_sys_wifi_signal_3_anim3.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ffff00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ffff00' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#ffff00' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#ffff00' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#ffff00'
+mkicon $TEMPLATE stat_sys_wifi_signal_3_anim4.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ffff00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ffff00' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#ffff00' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#ffff00'
+mkicon $TEMPLATE stat_sys_wifi_signal_3_anim5.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#ffff00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#ffff00' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#ffff00'
+
+mkicon $TEMPLATE stat_sys_wifi_signal_4_anim0.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#00ff00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#00ff00'
+mkicon $TEMPLATE stat_sys_wifi_signal_4_anim1.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#00ff00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#00ff00' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#00ff00'
+mkicon $TEMPLATE stat_sys_wifi_signal_4_anim2.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#00ff00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#00ff00' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#00ff00' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#00ff00'
+mkicon $TEMPLATE stat_sys_wifi_signal_4_anim3.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#00ff00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#00ff00' 'RING1' 'stroke:\#7b7b7b' 'stroke:\#00ff00' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#00ff00' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#00ff00'
+mkicon $TEMPLATE stat_sys_wifi_signal_4_anim4.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#00ff00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#00ff00' 'RING2' 'stroke:\#7b7b7b' 'stroke:\#00ff00' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#00ff00'
+mkicon $TEMPLATE stat_sys_wifi_signal_4_anim5.svg 'CENTER' 'fill:\#7b7b7b' 'fill:\#00ff00' 'CENTER' 'stroke:\#7b7b7b' 'stroke:\#00ff00' 'RING3' 'stroke:\#7b7b7b' 'stroke:\#00ff00'
+
